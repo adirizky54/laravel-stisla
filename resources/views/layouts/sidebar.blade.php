634cbb95d4e0f663862cbec1c695a2568368ebbf
Adi Rizky <adirizky54@gmail.com>
2020-05-27 13:59:34 +0700

    init

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,15 @@
+<aside id="sidebar-wrapper">
+  <div class="sidebar-brand">
+    <a href="javascript:void(0)">{{ env('APP_NAME') }}</a>
+  </div>
+  <div class="sidebar-brand sidebar-brand-sm">
+    <a href="javascript:void(0)">St</a>
+  </div>
+  <ul class="sidebar-menu">
+    <li>
+      <a class="nav-link" href="javascript:void(0)">
+        <i class="fas fa-home"></i> <span>Blank Page</span>
+      </a>
+    </li>
+  </ul>
+</aside>
